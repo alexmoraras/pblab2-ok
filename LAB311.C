dd077525b1929cb4ac1294133557f7e5ca7b0c7c
Soltuz_Stefan <fane@gmail.com>
2024-03-05 13:18:30 +0200

    am rezolvat greseala

--- LAB311.C
+++ LAB311.C
@@ -1,1 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int function(int x){
+	x=x-2;
+	if(x>1)
+        x=function(x);
+	return 30-x;
+}
+
+int main(){
+	int a=30, i=0;
+
+	i=function(a);
+	printf("%d", i);
+	return 0;
+}
+
 
